02ee06dd915807c0e097ee5eb60c43f26b04b59a
maxbond <ceo@rf.ru>
2023-10-03 23:07:05 +0300

    initial HTML for project

---
+++ app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and click  the "Find Names" button below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-buttons">
+          <button>Boy</button>
+          <button>Unisex</button>
+          <button>Girl</button>
+        </div>
+      </div>
+        <div class="option-container">
+          <h4>2) Popularity on a Name</h4>
+        <div class="option-buttons">
+          <button>Trendy</button>
+          <button>Unique</button>
+        </div>
+        </div>
+        <div class="option-container">
+          <h4>3) Choose the name's length</h4>
+        <div class="option-buttons">
+          <button>Long</button>
+          <button>All</button>
+          <button>Short</button>
+          
+        </div>
+        </div>
+    </div>
+  </div>
+</template>
