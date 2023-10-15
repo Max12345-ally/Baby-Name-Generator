58e1dc8449074e3f27a83129543e95fb3316ecd4
maxbond <ceo@rf.ru>
2023-10-15 17:24:18 +0300

    passing props to components

--- app.vue
+++ app.vue
@@ -51,8 +51,12 @@ interface OptionsState {
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click  the "Find Names" button below</p>
     <div class="options-container">
-      <Option v-for="option in optionsArray" :key="option.title" />
-        
+      <Option 
+      v-for="option in optionsArray" 
+      :key="option.title" 
+      :option="option"
+      :options="options"
+      />
         <button class="primary" @click="computeSelectedNames">Find names</button>
     </div>
     <div class="cards-container">
@@ -77,42 +81,7 @@ interface OptionsState {
     font-size: 3rem;
   }
 
-  .options-container {
-    background-color: rgb(255,238,236);
-    border-radius: 2rem;
-    padding: 1rem;
-    width: 95%;
-    margin: 0 auto;
-    margin-top: 4rem;
-  }
-
-  .option-container {
-    margin-bottom: 2rem;
-  }
-.option{
-  background-color: white;
-  outline: 0.15rem solid rgb(249,87,89);
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: rfb(27,60,138);
-  cursor: pointer;
-  font-weight: 200;
-}
-
-.option-left {
-  border-radius: 1rem 0 0 1rem;
-}
-
-.option-right {
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option-active {
-  background-color:rgb(249,87,89);
-  color:white
-}
+  
 
 .primary {
   background-color: rgb(249,87,89);;
